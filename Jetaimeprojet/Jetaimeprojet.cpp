db7f976acc7685e47efec7f375c802d5cdcb2990
Casey <cnelson-lassoie@artfx.fr>
2022-03-14 12:59:37 +0100

    J'ai vomit du sang

--- Jetaimeprojet/Jetaimeprojet.cpp
+++ Jetaimeprojet/Jetaimeprojet.cpp
@@ -5,6 +5,31 @@
 
 int main()
 {
-    std::cout << "\n\n\nYOU GONNA SPLIT BLOOD\n\nSPEW TOO\n\n\n\n";
+    std::cout << "\n\n\nYOU GONNA SPLIT BLOOD\n\nSPEW IT TOO\n\n\n\n";
     
+
+    using namespace std;
+
+    void Damage(int health) {
+        health -= 2;
+    }
+
+    void DamageRefefrence(int& health) {
+        health -= 2;
+    }
+
+    void DamagePointedObject(int* health) {
+        *health -= 2;
+    }
+
+    int main()
+    {
+        cout << "\n\n\nYOU GONNA SPLIT BLOOD\n\nSPEW IT TOO\n\n\n\n";
+        int life = 10;
+        cout << "The value " << life << " is stroed at memory adress :" << &life << endl;
+        
+
+
+
+    }
 }
