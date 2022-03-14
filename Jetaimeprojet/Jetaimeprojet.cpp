73dafe900a9bce23d4b373c74aeb1c012e88ff4a
unknown <cnelson-lassoie@artfx.fr>
2022-03-14 11:17:52 +0100

    JE VOMIS DU SANG

---
+++ Jetaimeprojet/Jetaimeprojet.cpp
@@ -0,0 +1,10 @@
+// Jetaimeprojet.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "\n\n\nYOU GONNA SPLIT BLOOD\n\nSPEW TOO\n\n\n\n";
+    
+}
